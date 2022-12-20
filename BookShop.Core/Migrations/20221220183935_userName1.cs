af67aee7c60eaad303b6757d6bd5256ae8e5be72
Aleksander Litvinenko <aleksanderndt@gmail.com>
2022-12-20 22:51:40 +0300

    *

---
+++ BookShop.Core/Migrations/20221220183935_userName1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShop.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class userName1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
